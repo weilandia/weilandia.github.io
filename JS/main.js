2aa6d7a2a0a3378690bc346aa11ff25917f7018a
Nick Weiland <nickweiland@gmail.com>
2016-01-21 23:34:23 -0700

    Add new site

--- JS/main.js
+++ JS/main.js
@@ -4,21 +4,9 @@ if (logoHeight < 104) {
     $('#myDiv img').css('margin-top', margintop);
 }
 
-$(function() {
-    var words = ['Hi', 'Hello', 'Hey'],
-        index = 0,
-        $el = $('#rotate-word')
-    setInterval(function() {
-        index++ < words.length - 1 || (index = 0);
-        $el.fadeOut(function() {
-            $el.text(words[index]).fadeIn();
-        });
-    }, 2500);
-});
-
-$(function(){
-    $(".element").typed({
-        strings: ["Hi.^1000 I'm Nick.", "It's a big world out there.^3000 \nLet's make it better."],
-        typeSpeed: 0
-    });
-});
+// $(function(){
+//     $(".element").typed({
+//         strings: ["Hi.^1000 I'm Nick.", "It's a big world out there.^3000 \nLet's make it better."],
+//         typeSpeed: 0
+//     });
+// });
