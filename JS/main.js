ddaf8b70305f0ec2e2cc7b6264626f8f1b642947
Nick Weiland <nickweiland@gmail.com>
2016-01-22 00:05:42 -0700

    Add new site

--- JS/main.js
+++ JS/main.js
@@ -3,10 +3,3 @@ if (logoHeight < 104) {
     var margintop = (104 - logoHeight) / 2;
     $('#myDiv img').css('margin-top', margintop);
 }
-
-// $(function(){
-//     $(".element").typed({
-//         strings: ["Hi.^1000 I'm Nick.", "It's a big world out there.^3000 \nLet's make it better."],
-//         typeSpeed: 0
-//     });
-// });
