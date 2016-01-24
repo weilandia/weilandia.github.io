445d5cabdd416e28cd054f4c446961ce7a4ee9d0
Nick Weiland <nickweiland@gmail.com>
2016-01-24 11:46:30 -0700

    Add resume

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    $(".element").typed({
+        strings: ["^300Hi.^1000 I'm Nick.^500", "It's a big world out there.^2000 \nLet's make it better."],
+        typeSpeed: 0
+    });
+});
