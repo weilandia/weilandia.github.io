9816255873984a750d6edebdfe88b8273c17626f
Nick Weiland <nickweiland@gmail.com>
2016-01-28 17:16:17 -0700

    Restructure 404

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,15 @@ var callback = function(){
         width: newWidth,
     }, 1000);
 	});
+
+	$('.item-skills-soft').each(function(){
+		newWidth = $(this).parent().width() * $(this).data('percent');
+		$(this).width(0);
+		$(this).animate({
+				width: newWidth,
+		}, 1000);
+	});
+
 	$('.icons-red').each(function(){
 		height = $(this).height();
     $(this).animate({
