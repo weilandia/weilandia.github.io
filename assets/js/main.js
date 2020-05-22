03fe5884d09fc0516f41defae1728b91be48d422
Nick Weiland <nickweiland@gmail.com>
2020-05-22 09:32:05 -0700

    Update

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $(".element").typed({
-        strings: ["^300Hi.^1000 I'm Nick.^500", "It's a big world out there.^2000 \nLet's make it better."],
-        typeSpeed: 0
-    });
-});
